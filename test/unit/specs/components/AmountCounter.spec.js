25e7013d28c348f840eff0255def9545b482f0e7
João Rigotti <joao.webdev@gmail.com>
2017-10-30 21:05:16 -0200

    Adicionado testes.

--- test/unit/specs/components/AmountCounter.spec.js
+++ test/unit/specs/components/AmountCounter.spec.js
@@ -2,11 +2,28 @@ import Vue from 'vue'
 import AmountCounter from '@/components/AmountCounter'
 
 describe('AmountCounter.vue', () => {
-  it('deve renderizar o componente corretamente', () => {
-    const Constructor = Vue.extend(AmountCounter)
-    const vm = new Constructor().$mount()
+  let vm = {}
 
+  beforeEach(() => {
+    const Constructor = Vue.extend(AmountCounter)
+    vm = new Constructor().$mount()
+  })
+  it('deve renderizar o componente corretamente', () => {
     expect(vm.$el.querySelectorAll('button').length).to.equal(2)
     expect(vm.$el.querySelectorAll('span').length).to.equal(1)
+  })
+
+  it('deve chamar o evento increment', () => {
+    const spy = sinon.spy()
+    vm.$on('increment', spy)
+    vm.increment()
+    expect(spy).to.have.been.called
+  })
+
+  it('deve chamar o evento decrement', () => {
+    const spy = sinon.spy()
+    vm.$on('decrement', spy)
+    vm.decrement()
+    expect(spy).to.have.been.called
   })
 })
